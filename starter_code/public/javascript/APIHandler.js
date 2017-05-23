f230f013372d979271f9f7ec5cba0afc3d247734
ernestques <ernesqr@yahoo.com>
2017-05-23 13:45:56 -0400

    first iteration

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
+function showFeedback(postResponse) {
+    console.log('working');
+    console.log(postResponse);
+}
+
+function handleError(err) {
+    console.log('Not working');
+    console.log(err);
+}
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
 
-  }
+    getFullList() {
+        $.ajax({
+            method: 'GET',
+            url: 'http://ih-api.herokuapp.com/characters',
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  getOneRegister () {
+    getOneRegister(one) {
+        $.ajax({
+            method: 'GET',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  }
+    createOneRegister(name,occupation,debt,weapon) {
+      
+      const character = {
+          name: name,
+          occupation: occupation,
+          debt: debt,
+          weapon: weapon
+      };
+        $.ajax({
+            method: 'POST',
+            url: 'http://ih-api.herokuapp.com/characters',
+            data: character,
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  createOneRegister () {
+    updateOneRegister(name,occupation,debt,weapon) {
+        $.ajax({
+            method: 'PATCH',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            data: {
+                name: name,
+                occupation: occupation,
+                debt: debt,
+                weapon: weapon
+            },
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(one) {
+        $.ajax({
+            method: 'DELETE',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            success: showFeedback,
+            error: handleError
+        });
+    }
 }
